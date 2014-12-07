949520601559f9f19dc028d9e73da769773e09ac
SuzeShardlow <suzeplays@gmail.com>
2014-12-07 10:10:14 -0800

    Added js functions
    Added all the functions.

--- guess.js
+++ guess.js
@@ -1,1 +1,18 @@
 // TODO 
+function computerNumber() {
+    // This function will make the computer think of a number between 1 and 100
+}
+
+function numberCheck(userGuess, minimum, maximum) {
+    // This function checks that the guess is between the minimum and maximum
+    if (userGuess >= minimum && userGuess <= maximum) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function userGuessed() {
+    // This function gets the number that the user guessed and compares it to the number the computer thought of
+}
+ 
