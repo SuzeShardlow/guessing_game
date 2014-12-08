092626d01cb0313e9e25afa83c7d32a5d483b3a7
SuzeShardlow <suzeplays@gmail.com>
2014-12-08 10:46:09 -0800

    Validation and checking
    Generates the secret number, checks that the guess is valid and if so
    compares it against the secret number.

--- guess.js
+++ guess.js
@@ -1,10 +1,14 @@
-// TODO 
-function computerNumber() {
+// TODO
+var secretNumber;
+
+function computeNumber() {
     // This function will make the computer think of a number between 1 and 100
+    return Math.floor(Math.random() * 100 + 1);
 }
 
 function numberCheck(userGuess, minimum, maximum) {
     // This function checks that the guess is between the minimum and maximum
+    // TODO make sure it's a whole number
     if (userGuess >= minimum && userGuess <= maximum) {
         return true;
     } else {
@@ -14,5 +18,27 @@ function numberCheck(userGuess, minimum, maximum) {
 
 function userGuessed() {
     // This function gets the number that the user guessed and compares it to the number the computer thought of
+    var userNumber = document.getElementById("userGuess").value;
+    if(numberCheck(userNumber,1,100)) {
+        if(userNumber>secretNumber) {
+            // Guess too high
+            alert("Your guess is too high.");
+        } else {
+            if(userNumber<secretNumber) {
+                // Guess too low
+                alert("Your guess is too low.");
+            } else {
+                // Guess is correct
+                alert("Your guess is correct.");
+            }
+        }
+    } else {
+        alert("Your guess needs to be between 1 and 100.");
+    }
 }
- 
+
+window.onload = function() {
+    document.getElementById("guessButton").onclick=userGuessed;
+    secretNumber=computeNumber();
+    console.log(secretNumber);
+}
